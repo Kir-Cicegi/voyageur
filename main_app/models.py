5fd791fe2b3102cbacec0eafa15d0ce64ddf8cc6
Kir-Cicegi <84298571+Kir-Cicegi@users.noreply.github.com>
2021-07-02 21:57:04 -0400

    Collector - Part 1

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class City:
+  def __init__(self, name, country, fav_spot, fav_local_food, days_spent):
+    self.name = name
+    self.country = country
+    self.fav_spot = fav_spot
+    self.fav_local_food = fav_local_food
+    self.days_spent = days_spent
+
+
+cities = [
+  City('Tuscany', 'Italy','Antinori winery', 'burrata', 15),
+  City('Galway', 'Ireland','Busker Brownes', 'soda bread', 7),
+  City('New York', 'USA', 'Stone Street', 'pretzel', 15)
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .models import *
+from .forms import *
 # Create your views here.
+
+def home (request):
+    return render(request, 'index.html', {'cities': cities})
+
+def about(request):
+    return render(request, 'about.html')
+
+
+
+def city_list(request):
+    return render(request, 'cities/index.html', { 'cities': cities })
