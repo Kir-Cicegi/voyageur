51f9de22092fdd9cb978cd27983dbe44d5032d14
Kir-Cicegi <84298571+Kir-Cicegi@users.noreply.github.com>
2021-07-05 20:42:00 -0400

    Collector - Part 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class City:
-  def __init__(self, name, country, fav_spot, fav_local_food, days_spent):
-    self.name = name
-    self.country = country
-    self.fav_spot = fav_spot
-    self.fav_local_food = fav_local_food
-    self.days_spent = days_spent
+class City(models.Model):
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    fav_spot = models.CharField(max_length=200)
+    fav_local_food = models.CharField(max_length=200)
+    days_spent = models.IntegerField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
 
+ 
 
-cities = [
-  City('Tuscany', 'Italy','Antinori winery', 'burrata', 15),
-  City('Galway', 'Ireland','Busker Brownes', 'soda bread', 7),
-  City('New York', 'USA', 'Stone Street', 'pretzel', 15)
-]
+    # renames the instances of the model
+        # with their title name
+    def __str__(self):
+        return self.name
+
+# cities = [
+#   City('Tuscany', 'Italy','Antinori winery', 'burrata', 15),
+#   City('Galway', 'Ireland','Busker Brownes', 'soda bread', 7),
+#   City('New York', 'USA', 'Stone Street', 'pretzel', 15)
+# ]
+
+   # def __init__(self, name, country, fav_spot, fav_local_food, days_spent):
+    #     self.name = name
+    #     self.country = country
+    #     self.fav_spot = fav_spot
+    #     self.fav_local_food = fav_local_food
+    #     self.days_spent = days_spent
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from .forms import *
 # Create your views here.
 
 def home (request):
-    return render(request, 'index.html', {'cities': cities})
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -13,4 +13,27 @@ def about(request):
 
 
 def city_list(request):
+    # City.objects.create(name="Tuscany", country="Italy", fav_spot="Antinori Winery", fav_local_food="Burrata", days_spent=15)
+    cities = City.objects.all()
     return render(request, 'cities/index.html', { 'cities': cities })
+
+def detail(request, city_id):
+    city = City.objects.get(id=city_id)
+    return render(request, 'cities/detail.html', { 'city': city })
+
+
+
+def new_form (request):
+    
+    if request.method == 'POST':
+        print("I have received form data")
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/cities/')
+    else: 
+        form = CityForm()
+
+        return render(request, 'cities/form.html', {'form': form})
+
+
